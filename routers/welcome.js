b9fb64b7ceba624dd462cb4b2aa867a63f7d9fb1
Jason Maurer <jason@maur.co>
2019-12-05 20:15:06 -0700

    Typo fix

--- routers/welcome.js
+++ routers/welcome.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-  res.send("<h2>Welcome to the Lambda Hubs API</h>")
+  res.send("<h2>Welcome to the Lambda Hubs API</h2>")
 })
 
 router.get("/api", (req, res) => {
